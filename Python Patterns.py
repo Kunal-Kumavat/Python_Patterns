6943d5cd79f64a8f9f03d3cc5b185b63b365953b
Kunal-Kumavat <97896771+Kunal-Kumavat@users.noreply.github.com>
2024-03-05 23:27:35 +0530

    Add files via upload

--- Python Patterns.py
+++ Python Patterns.py
@@ -552,3 +552,24 @@ def pattern(word):
         print(word[0:i])
 
 pattern(str(input("Enter Word :: ")))
+
+
+
+# A 
+# B C 
+# D E F 
+# G H I J 
+# K L M N O 
+# P Q R S T U 
+# V W X Y Z [ \
+asciiNumber = 65
+def pattern(height):
+    global asciiNumber
+    for i in range(1,height+1):
+        for k in range(1,i+1):
+            print(chr(asciiNumber),end=' ')
+            asciiNumber += 1
+        print('')
+    
+height = int(input("Enter Pattern Height :: "))
+pattern(height)
